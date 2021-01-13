3d00e91e502e545e85bba2bcab90a8d442c46583
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-12 23:06:03 -0600

    RoseForecast component created

---
+++ client/components/RoseForecast.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RoseForecast = () => {
+  return (
+    <div>
+      Salt Lake Avalanche Forecast:
+      <div>
+        <img src='https://utahavalanchecenter.org/sites/default/files/forecast/202012/20201220-062339-3.png' alt='Avalanche forecase rose (Salt Lake)' />
+      </div>
+    </div>
+  );
+};
+
+export default RoseForecast;
