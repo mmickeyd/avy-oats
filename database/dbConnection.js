d2adb99f878409e3e5de639ba0556612585a4d76
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-12 22:59:11 -0600

    database configured

---
+++ database/dbConnection.js
@@ -0,0 +1,10 @@
+const config = require('./config.js');
+const { Pool } = require('pg');
+
+const pool = new Pool(config);
+
+const dbConnection = pool.connect()
+  .then( console.log('Postgres database connected') )
+  .catch( e => console.error(e) );
+
+module.exports = dbConnection;
