0bbf33cfed47d4d215e6c83360d1eafbe2555172
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-13 23:38:54 -0600

    morgan npm package utilized

--- server/clientRoutes.js
+++ server/clientRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const client = express();
 const controller = require('./controllers');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 client.use(express.static(__dirname + '/../client/dist'));
 client.use(bodyParser.urlencoded({ extended: true }));
 client.use(bodyParser.json());
+client.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 client.get('/forecasts', controller.getForecast);
 client.put('/forecasts', controller.updateForecast);
