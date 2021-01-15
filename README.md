979c529229459b1b34b347638abc8b71d6ab3b3d
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-14 22:14:13 -0600

    general webpage styling complete

--- README.md
+++ README.md
@@ -2,15 +2,43 @@
 Avy Oats
 
 ## Description ##
+
 > All-in-one app providing an overview for decision making for backcountry skiers of the Wasatch. Made for those early dawn patrol mornings, but can be useful anytime of the day. This eliminates the need to check several website for all the information needed before heading out for a tour. Future iterations will include extended forecasts, graphs, and much more!
 
-## Sections to App ##
+> A portion of this app uses information pulled from the UAC (Utah Avalanche Center) and it would not be nearly as useful without their free contributions. If you enjoy using this app, please check out their website and consider donating to the cause. [UAC Donation Page](https://utahavalanchecenter.org/)
+
+## Sections to App ## (not all complete yet)
 > Avalanche forecasts
-> | Cloud cover forecasts
-> | UDOT traffic cams
-> | Animated radar covering the last hour
-> | Detailed weather forecasts
-> | Graphs showing data from weather stations
+> Cloud cover forecasts
+> UDOT traffic cams
+> Animated radar covering the last hour
+> Detailed weather forecasts
+> Graphs showing data from weather stations
+
+## Creating environment variables and database ##
+
+1. Create .env file, following .env_sample template
+
+2. To create the database locally:
+```
+psql postgres < database/schema.sql
+```
+
+###### OR ######
+
+2. To create the database on Ubuntu:
+```
+sudo apt update
+sudo apt install postgres postgresql-client
+sudo -u postgres psql < database/schema.sql
+```
+
+3. Insert a fake forecast into database:
+```
+sudo -u postgres psql
+\c avyoats
+insert into forecasts (forecast) values ('not a real forecast');
+```
 
 ## Installing Dependencies and Usage ##
 
@@ -26,10 +54,5 @@ npm run build
 
 3. To start the server:
 ```
-npm run start
-```
-
-4. To create the database:
-```
-psql postgres < database/schema.sql
+npm start
 ```
--- client/App.jsx
+++ client/App.jsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import oats from './dist/oats.png';
+import logo from './dist/avyOatsLogo.png';
 import RoseForecast from './components/RoseForecast.jsx';
 import UdotTrafficCams from './components/UdotTrafficCams.jsx';
 import UacForecast from './components/UacForecast.jsx';
 import Radar from './components/Radar.jsx';
 import CloudCoverForecast from './components/CloudCoverForecast.jsx';
 import Webcams from './components/Webcams.jsx';
-import AltaWxStations from './components/AltaWxStations.jsx';
 import TwitterFeed from './components/TwitterFeed.jsx';
+
+import styles from './css/generalStyles.css';
 
 const App = () => {
   const [forecast, setForecast] = useState('');
@@ -23,18 +25,26 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <img src={oats} alt='oats'/>
-      <h1>Avy Oats</h1>
-      <img src={oats} alt='oats'/>
-      <RoseForecast avyRose={forecast.overall_danger_rose_image} />
-      <UacForecast forecast={forecast} />
-      <h2>Traffic Cams</h2>
-      <UdotTrafficCams canyon={'lcc'} />
-      <UdotTrafficCams canyon={'bcc'} />
-      <Webcams />
-      <Radar />
-      <CloudCoverForecast />
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img className={styles.oatsFlipped} src={oats} alt='oats'/>
+        <h1 className={styles.avyOatsText}>Avy Oats</h1>
+        <img className={styles.oats} src={oats} alt='oats'/>
+      </div>
+      <RoseForecast className={styles.roseForecast} avyRose={forecast.overall_danger_rose_image}/>
+      <UacForecast className={styles.uacForecast} forecast={forecast}/>
+      <div className={styles.trafficCams}>
+        <h2>Traffic Cams</h2>
+        <span>
+          <UdotTrafficCams canyon={'lcc'} />
+          <UdotTrafficCams canyon={'bcc'} />
+        </span>
+      </div>
+      <Webcams className={styles.webcams}/>
+      <div className={styles.radarCloudCover}>
+        <Radar className={styles.radar}/>
+        <CloudCoverForecast className={styles.cloudCover}/>
+      </div>
       <TwitterFeed />
     </div>
   );
