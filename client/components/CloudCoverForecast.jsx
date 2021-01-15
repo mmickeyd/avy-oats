577d6b8e311e534162946901e6b818b25f58303d
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-14 18:23:48 -0600

    CloudCoverForecast refactored

--- client/components/CloudCoverForecast.jsx
+++ client/components/CloudCoverForecast.jsx
@@ -14,18 +14,19 @@ const CloudCoverForecast = () => {
     let rotator = document.getElementById('rotator');
     let delayInSeconds = 1;
 
-    let images = ['https://graphical.weather.gov/images/utah/Sky1_utah.png', 'https://graphical.weather.gov/images/utah/Sky3_utah.png', 'https://graphical.weather.gov/images/utah/Sky5_utah.png', 'https://graphical.weather.gov/images/utah/Sky7_utah.png', 'https://graphical.weather.gov/images/utah/Sky9_utah.png'];
+    let images = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     let num = 0;
     let changeImage = function() {
       let length = images.length;
-      rotator.src = images[num++];
+      rotator.src = `https://graphical.weather.gov/images/utah/Sky${images[num++]}_utah.png`;
       if (num === length) {
         num = 0;
       }
     };
     setInterval(changeImage, delayInSeconds * 1000);
   };
+
   window.onload = () => {
     start();
   };
